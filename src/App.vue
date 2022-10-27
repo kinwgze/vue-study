04d8049b23b0970a407ae4c7992ebcd1ce3deb6f
zeeew <wz297443228@outlook.com>
2022-10-27 19:31:46 +0800

    2022.10.27 【1】

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,97 @@
+<!--
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div class="container">
+    我是根组件
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App'
+}
+</script>
+-->
 
+<!--选项api
+<template>
+  <div class="container">
+    <div>鼠标位置：</div>
+    <div>X轴：{{x}}</div>
+    <div>Y轴：{{y}}</div>
+    <hr>
+    <div>{{count}} <button @click="add()">自增</button></div>
+  </div>
+</template>
+<script>
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      x: 0,
+      y: 0,
+      count: 0
+    }
+  },
+  mounted() {
+    document.addEventListener('mousemove', this.move)
+  },
+  methods: {
+    move(e) {
+      this.x = e.pageX
+      this.y = e.pageY
+    },
+    add () {
+      this.count++
+    }
+  },
+  destroyed() {
+    document.removeEventListener('mousemove', this.move)
+  }
+}
+</script>-->
+
+<!--组合api-->
+<template>
+  <div class="container">
+    <div>鼠标位置：</div>
+    <div>X轴：{{x}}</div>
+    <div>Y轴：{{y}}</div>
+    <hr>
+    <div>{{count}} <button @click="add()">自增</button></div>
+  </div>
+</template>
+<script>
+import { onMounted, onUnmounted, reactive, ref, toRefs } from 'vue'
+export default {
+  name: 'App',
+  setup () {
+    // 鼠标移动逻辑
+    const mouse = reactive({
+      x: 0,
+      y: 0
+    })
+    const move = e => {
+      mouse.x = e.pageX
+      mouse.y = e.pageY
+    }
+    onMounted(()=>{
+      document.addEventListener('mousemove',move)
+    })
+    onUnmounted(()=>{
+      document.removeEventListener('mousemove',move)
+    })
+
+    // 累加逻辑
+    const count = ref(0)
+    const add = () => {
+      count.value ++
+    }
+
+    // 返回数据
+    return {
+      ...toRefs(mouse),
+      count,
+      add
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+// 主要职责：创建一个vue应用
+// 1. 导入createApp函数，从vue中
+// 2. 创建一个跟组件App.vue，导入至main
+// 3. 使用createApp创建应用实例
+// 4. 应用实例挂载到#app容器中
 
-createApp(App).mount('#app')
+
+import {createApp} from 'vue'
+import App from './App.vue'
+const app = createApp(App)
+app.mount('#app')
+
