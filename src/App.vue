c0772c5f079372a5f5247ec83872416882d73fd8
zeeew <wz297443228@outlook.com>
2022-11-15 19:44:46 +0800

    2022.11.15 【1】

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="container">
+    <h1>父组件</h1>
+    <p>{{money}}</p>
+    <hr>
+    <Son :money = "money"/>
   </div>
 </template>
 <script>
+import Son from "./Son.vue";
+import {ref} from "vue";
 export default {
   name: 'App',
+  components: {
+    Son
+  },
+  // 父组件的数据传递为子组件
+  setup() {
+    const money = ref(100)
+    return {money}
+  }
 }
 </script>
---
+++ src/Son.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1>子组件</h1>
+    <p>{{money}}</p>
+  </div>
+</template>
+<script>
+import { onMounted } from 'vue'
+export default {
+  name: 'Son',
+  // 子组件接收父组件数据使用props即可
+  props: {
+    money: {
+      type: Number,
+      default: 0
+    }
+  },
+  setup (props) {
+    // 获取父组件数据money
+    console.log(props.money)
+  }
+}
+</script>
